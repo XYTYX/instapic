09a663bcf2e318a59903bc989bf707f27a36f913
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-03 21:27:34 +0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserHistory } from "history";
+import React, { useState } from "react";
+import { Route, Router, Switch } from "react-router";
+import { Link } from "react-router-dom";
+import { Api, ApiImpl } from "./api";
+import { Input } from "./api/components";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const customHistory = createBrowserHistory();
+  const api = ApiImpl("http://localhost:5000");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={customHistory}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Explore</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login">
+            <Login api={api} />
+          </Route>
+          <Route path="/">
+            <Explore />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Explore() {
+  return <h2>Home</h2>;
+}
+
+interface LoginInterface {
+  api: Api;
+}
+
+function Login(props: LoginInterface) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  async function onSubmit() {
+    const response = await props.api.login(email, password);
+    console.log(response.authorization);
+  }
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={onSubmit}>
+        <Input value={email} onChange={setEmail}></Input>
+        <Input type="password" value={password} onChange={setPassword}></Input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
