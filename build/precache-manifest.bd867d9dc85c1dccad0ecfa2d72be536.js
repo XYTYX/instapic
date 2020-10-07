cb8eff38a560f1bcccb7b00be18ac30ef9d51011
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-07 20:33:08 +0800

    build 2

---
+++ build/precache-manifest.bd867d9dc85c1dccad0ecfa2d72be536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2472637301b3b7e514bdc15a12866973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f6680adc5d021fffe6",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "cb6df12471c6ef8350a0",
+    "url": "/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "40f6680adc5d021fffe6",
+    "url": "/static/js/2.108c2916.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.108c2916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6df12471c6ef8350a0",
+    "url": "/static/js/main.ac7ee425.chunk.js"
+  },
+  {
+    "revision": "8f078322a8fd4e7688a3",
+    "url": "/static/js/runtime-main.1317199a.js"
+  }
+]);
